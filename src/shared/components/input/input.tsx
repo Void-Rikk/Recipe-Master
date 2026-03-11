3fd896ccf04b4fc7d8ec288336f089555113b7e6
Denis <buddy.den@mail.ru>
2026-03-11 15:46:02 +0300

    UI: input

---
+++ src/shared/components/input/input.tsx
@@ -0,0 +1,32 @@
+import type { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
+import { cva, type VariantProps } from "class-variance-authority";
+
+const inputVariants = cva(
+    "py-2 px-4 text-white rounded-lg",
+    {
+        variants: {
+            variant: {
+                default: "bg-gray-600",
+                destructive: "bg-red-600",
+                primary: "bg-teal-600"
+            },
+            size: {
+                default: ""
+            }
+        },
+        defaultVariants: {
+            variant: "default",
+            size: "default"
+        }
+    }
+);
+
+function Input( { className, variant, size, ...props }: ComponentProps<"input"> & VariantProps<typeof inputVariants> ) {
+
+    return (
+        <input { ...props } className={ twMerge(inputVariants({ variant, size, className })) }/>
+    );
+}
+
+export default Input;
