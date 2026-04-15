0ba089b5489ecded8e36bf2d0090fa8e502b4d84
Denis <buddy.den@mail.ru>
2026-04-15 21:22:43 +0300

    Create Recipe Page: design fix

--- src/pages/create-recipe-page/create-recipe-page.tsx
+++ src/pages/create-recipe-page/create-recipe-page.tsx
@@ -6,8 +6,8 @@ import { CreateRecipeForm } from "../../modules/create-recipe-form";
 function CreateRecipePage() {
 
     return (
-        <div className="flex flex-col gap-4 w-[70%] pb-4 h-[100vh]
-            rounded-md
+        <div className="flex flex-col gap-4 w-[55%] h-[100vh]
+            rounded-md mt-2 mb-2
             shadow-[1px_1px_5px_1px] shadow-gray-300
             max-md:w-full max-md:mt-0 max-md:rounded-none max-md:shadow-none max-md:h-min"
         >
